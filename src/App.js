98ed7825615acbf99438585f870866e2d2ab65ba
vijay0221 <vijaymaroju2003@gmail.com>
2023-07-20 14:19:52 +0530

    implimented lifting up state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 export default function App(){
+  const [items,setItems]=useState([]); 
+   function handleAddItems(item){
+  setItems(items=>[...items,item])
+
+ }
+
+
  return <div className="app">
   <Logo/>
-  <Form />
-  <PackingList/>
+  <Form onAddItems={handleAddItems} />
+  <PackingList items={items}/>
   <Stats/>
  </div>
 
@@ -21,16 +29,20 @@ function Logo(){
 }
 
 
-function Form(){
+function Form({onAddItems}){
 
   const [description,setDescription]=useState('');
   const [quantity, setQuantity]=useState(1);
+
+
 
   function handleSubmit(e){
     e.preventDefault();
     if(!description) return //guard class
     const newItem={description,quantity,packed:false,id:Date.now()}
     console.log(newItem);
+
+    onAddItems(newItem)
 
     setDescription('');
     setQuantity(1);
@@ -49,11 +61,11 @@ function Form(){
  </form>
 }
 
-function PackingList(){
+function PackingList({items}){
   return <div className="list">
    <ul>
     {
-      initialItems.map(item=><Item item={item} key={item.id}/>)
+      items.map(item=><Item item={item} key={item.id}/>)
     }
   </ul>
   </div>
